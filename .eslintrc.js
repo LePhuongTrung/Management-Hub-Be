96d15edf517152b00c94821413a3aa49582f384a
LePhuongTrung <trunglp@stisolutions.net>
2023-08-28 22:09:36 +0700

    handler eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,26 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'import',
+    'nestjs',
+    'security',
+    'clean-code',
+    'sonarjs',
+  ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
+    'prettier',
     'plugin:prettier/recommended',
     'plugin:jest/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:nestjs/recommended',
+    'plugin:security/recommended',
+    'plugin:sonarjs/recommended',
   ],
   root: true,
   env: {
@@ -26,10 +39,43 @@ module.exports = {
     '*.json',
   ],
   rules: {
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling'],
+          'index',
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
     'prettier/prettier': ['error', {}, { usePrettierrc: true }],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-var-requires': 'error',
+    '@typescript-eslint/ban-ts-comment': 'warn',
+    '@typescript-eslint/ban-types': 'warn',
+    'nestjs/parse-int-pipe': 'error',
+    'nestjs/deprecated-api-modules': 'error',
+    'nestjs/use-dependency-injection': 'error',
+    'nestjs/use-validation-pipe': 'error',
+    'clean-code/feature-envy': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
   },
 };
