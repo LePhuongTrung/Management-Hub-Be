4c5c9ac136c00cec2ec72e6a8376cc0e41ad02ad
LePhuongTrung <135190672+LPTrungGTN@users.noreply.github.com>
2023-10-08 17:20:56 +0700

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import config from '@config/app.config';
 import { AppModule } from '@src/app.module';
@@ -8,8 +9,22 @@ import { AppModule } from '@src/app.module';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   configureApp(app);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Example API')
+    .setDescription('The example API description')
+    .setVersion('1.0')
+    .addTag('example')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(config().port);
   console.log(`Server is running on http://localhost:${config().port}`);
+  console.log(
+    `Swagger UI is available at http://localhost:${config().port}/api-docs`,
+  );
 }
 
 function configureApp(app: INestApplication): void {
